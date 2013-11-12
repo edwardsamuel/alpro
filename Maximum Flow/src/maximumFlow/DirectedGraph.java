fcfb4f8da0b5d240af4242244c28f9606849ff61
raymond <raymond_pluto@hotmail.com>
2013-11-12 20:12:12 +0700

    maximum flow

---
+++ Maximum Flow/src/maximumFlow/DirectedGraph.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maximumFlow;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * This class is represents a directed graph for the maximum flow calculation
+ * 
+ * @author Ruben Beyer
+ */
+public class DirectedGraph {
+	private HashMap<Object, Node> nodes = new HashMap<>();
+	private LinkedList<Edge> edges = new LinkedList<>();
+
+	/**
+	 * Use this method to build the graph. It will add an edge to the graph and
+	 * also its nodes, if necessary. The node identifiers can be any object. Two
+	 * objects identify the same node, if they are equal according to their
+	 * equals function.
+	 * 
+	 * @param startNodeID
+	 *            Identifier object of the start node of the edge
+	 * @param endNodeID
+	 *            Identifier object of the end node of the edge
+	 * @param capacity
+	 *            Capacity of the edge
+	 */
+	void addEdge(Object startNodeID, Object endNodeID, int capacity) {
+		Node startNode;
+		Node endNode;
+		if (!this.nodes.containsKey(startNodeID)) {
+			startNode = new Node();
+			this.nodes.put(startNodeID, startNode);
+		} else {
+			startNode = this.nodes.get(startNodeID);
+		}
+		if (!this.nodes.containsKey(endNodeID)) {
+			endNode = new Node();
+			this.nodes.put(endNodeID, endNode);
+		} else {
+			endNode = this.nodes.get(endNodeID);
+		}
+		Edge edge = new Edge(startNodeID, endNodeID, capacity);
+		startNode.addEdge(edge);
+		endNode.addEdge(edge);
+		this.edges.add(edge);
+	}
+
+	Node getNode(Object nodeID) {
+		return this.nodes.get(nodeID);
+	}
+
+	LinkedList<Edge> getEdges() {
+		return this.edges;
+	}
+}
---
+++ Maximum Flow/src/maximumFlow/Node.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maximumFlow;
+import java.util.ArrayList;
+
+/**
+ * The node class that is used for DirectedGraph
+ * 
+ * @author Ruben Beyer
+ */
+
+public class Node {
+
+	private ArrayList<Edge> edges = new ArrayList<Edge>();
+
+	void addEdge(Edge edge) {
+		this.edges.add(edge);
+	}
+
+	Edge getEdge(int number) {
+		if (this.edges.size() <= number) {
+			return null;
+		} else {
+			return this.edges.get(number);
+		}
+	}
+
+	int getOutLeadingOrder() {
+		return this.edges.size();
+	}
+
+}
